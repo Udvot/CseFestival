bd44cac7a39006096a752490cd5dee8596ce50bc
alvi17 <alvi1005017@gmail.com>
2015-05-08 10:00:10 +0600

    Added maain

---
+++ src/com/udvot/udvot_fest/MainActivity.java
@@ -0,0 +1,45 @@
+package com.udvot.udvot_fest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity implements OnClickListener{
+	EditText username,password;
+	Button login,signup;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.login);
+		username=(EditText)findViewById(R.id.usernameeditText);
+		
+		password=(EditText)findViewById(R.id.passwordeditText);
+		
+		login=(Button)findViewById(R.id.loginbutton);
+		login.setOnClickListener(this);
+		signup=(Button)findViewById(R.id.signbutton);
+		signup.setOnClickListener(this);
+		
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		int id=v.getId();
+		switch(id)
+		{
+		case R.id.loginbutton:
+			//check login
+			break;
+		case R.id.signbutton:
+			//sign up
+			break;
+			
+		}
+	}
+
+}
